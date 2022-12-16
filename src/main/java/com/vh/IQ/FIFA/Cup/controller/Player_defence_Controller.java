b6c1813903b28a7afc7dfd11055161db13c3810b
Vishal9662 <104097927+Vishal9662@users.noreply.github.com>
2022-12-16 17:34:06 +0530

    Update Player_defence_Controller.java

--- src/main/java/com/vh/IQ/FIFA/Cup/controller/Player_defence_Controller.java
+++ src/main/java/com/vh/IQ/FIFA/Cup/controller/Player_defence_Controller.java
@@ -24,50 +24,45 @@ public class Player_defence_Controller {
 	private Player_defence_Service service;
 
 	/*	For Get All Data  */
-	@RequestMapping("/get/AllDetails")
+	@RequestMapping("/get/All")
 	public List<Player_defence> getAllData(){
 		log.info("This is the Information from Player_Defence_Controller class",Player_defence_Controller.class);
-		return service.getAllData();
+		return this.service.getAllData();
 	}
 	
 	/*	For Get Data By Id  */
 	@RequestMapping("/get/ById/{id}")
-	public Optional<Player_defence> getById(@PathVariable("id") int id){
+	public Optional<Player_defence> getDataById(@PathVariable("id") int id){
 		log.info("This is the Information from Player_Defence_Controller class",Player_defence_Controller.class);
-		Optional<Player_defence> op=service.getById(id);
-		return op;
+		return this.service.getDataById(id);
 	}
 	
 	/*	For Filter Data By position  */
 	@RequestMapping("/apply-filter/getBy/Position/{position}")
-	public ResponseEntity<List<Player_defence>> getByPosition(@PathVariable("position") String position){
+	public List<Player_defence> getByPosition(@PathVariable("position") String position){
 		log.info("This is the Information from Player_Defence_Controller class",Player_defence_Controller.class);
-		List<Player_defence> list=service.getByPosition(position);
-		return ResponseEntity.status(200).body(list);
+		return this.service.getByPosition(position);
 	}
 	
 	/*	For Filter Data By Team  */
 	@RequestMapping("/apply-filter/getBy/Team/{team}")
-	public ResponseEntity<List<Player_defence>> getByTeam(@PathVariable("team") String team){
+	public List<Player_defence> getByTeam(@PathVariable("team") String team){
 		log.info("This is the Information from Player_Defence_Controller class",Player_defence_Controller.class);
-		List<Player_defence> list=service.getByTeam(team);
-		return ResponseEntity.status(200).body(list);
+		return this.service.getByTeam(team);
 	}
 	
-	
-	/*	For Filter Data By no Of Tackles  */
+	/*	For Filter Data By Tackles  */
 	@RequestMapping("/apply-filter/getBy/Tackles/{tackles}")
-	public ResponseEntity<List<Player_defence>> getByTackles(@PathVariable("tackles") int tackles){
+	public List<Player_defence> getByTackles(@PathVariable("tackles") int tackles){
 		log.info("This is the Information from Player_Defence_Controller class",Player_defence_Controller.class);
-		List<Player_defence> list=service.getByTackles(tackles);
-		return ResponseEntity.status(200).body(list);
+		return this.service.getByTackles(tackles);
 	}
 	
-	/*	For Filter Data By no Of Won Tackles  */
-	@RequestMapping("/apply-filter/getBy/TacklesWon/{tackles_won}")
-	public ResponseEntity<List<Player_defence>> getByWonTackles(@PathVariable("tackles_won") int tackles_won){
+	/*	For Filter Data By Won Tackles  */
+	@RequestMapping("/apply-filter/getBy/WonTackles/{wontackles}")
+	public List<Player_defence> getByWonTackles(@PathVariable("wontackles") int wontackles){
 		log.info("This is the Information from Player_Defence_Controller class",Player_defence_Controller.class);
-		List<Player_defence> list=service.getByTacklesWon(tackles_won);
-		return ResponseEntity.status(200).body(list);
+		return this.service.getByTacklesWon(wontackles);
 	}
+	
 }
