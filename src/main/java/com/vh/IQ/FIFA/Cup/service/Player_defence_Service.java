9647900c2dbe61a6dd2cfbf2951db233a47af30b
Vishal9662 <104097927+Vishal9662@users.noreply.github.com>
2022-12-16 17:37:26 +0530

    Update Player_defence_Service.java

--- src/main/java/com/vh/IQ/FIFA/Cup/service/Player_defence_Service.java
+++ src/main/java/com/vh/IQ/FIFA/Cup/service/Player_defence_Service.java
@@ -3,6 +3,11 @@ package com.vh.IQ.FIFA.Cup.service;
 import java.util.List;
 import java.util.Optional;
 
+package com.vh.IQ.FIFA.Cup.service;
+
+import java.util.List;
+import java.util.Optional;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -24,32 +29,35 @@ public class Player_defence_Service {
 	}
 	
 	/*	For Get Data By Id  */
-	public Optional<Player_defence> getById(int id){
-		Optional<Player_defence> p1=repository.findById(id);
-		return p1;
+	public Optional<Player_defence> getDataById(int id)
+	{
+		Optional<Player_defence> list=repository.findById(id);
+		return list;
 	}
 	
 	/*	For Filter Data By Position */
 	public List<Player_defence> getByPosition(String position){
-		List<Player_defence> list=repository.findDataByPosition(position);
+		List<Player_defence> list=repository.findByPosition(position);
 		return list;
 	}
 	
 	/*	For Filter Data By Team name */
 	public List<Player_defence> getByTeam(String team){
-		List<Player_defence> list=repository.findDataByTeam(team);
+		List<Player_defence> list=repository.findByTeam(team);
 		return list;
 	}
 	
 	/*	For Filter Data By no Of tackles  */
 	public List<Player_defence> getByTackles(int tackles){
-		List<Player_defence> list=repository.findDataByTackles(tackles);
+		List<Player_defence> list=repository.findByTackles(tackles);
 		return list;
 	}
 	
 	/*	For Filter Data By no Of won the tackles  */
-	public List<Player_defence> getByTacklesWon(int tackles_won){
-		List<Player_defence> list=repository.findDataByTacklesWon(tackles_won);
+	public List<Player_defence> getByTacklesWon(int wontackles){
+		List<Player_defence> list=repository.findByWontackles(wontackles);
 		return list;
 	}
+	
+	
 }
