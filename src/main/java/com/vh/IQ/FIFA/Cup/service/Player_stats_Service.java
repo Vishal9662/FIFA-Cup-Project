44e6b0f4f574b89b2acb52b5c5b8ac13e88a10eb
Vishal9662 <104097927+Vishal9662@users.noreply.github.com>
2022-12-16 16:34:58 +0530

    Update Player_stats_Service.java

--- src/main/java/com/vh/IQ/FIFA/Cup/service/Player_stats_Service.java
+++ src/main/java/com/vh/IQ/FIFA/Cup/service/Player_stats_Service.java
@@ -9,6 +9,17 @@ import org.springframework.stereotype.Service;
 import com.vh.IQ.FIFA.Cup.entity.Player_stats;
 import com.vh.IQ.FIFA.Cup.repository.Player_stats_Repository;
 
+@Servicepackage com.vh.IQ.FIFA.Cup.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.vh.IQ.FIFA.Cup.entity.Player_stats;
+import com.vh.IQ.FIFA.Cup.repository.Player_stats_Repository;
+
 @Service
 public class Player_stats_Service{
 
@@ -29,28 +40,28 @@ public class Player_stats_Service{
 	
 	/*	For Filter Data By no Of games played  */
 	public List<Player_stats> getAllDataByGames(int games) {
-		List<Player_stats> list= repository.findDataByGames(games);
+		List<Player_stats> list= repository.findByGames(games);
 		return list;
 	}
 
 	/*	For Filter Data By no Of minutes played  */
 	public List<Player_stats> getAllDataByMinutes(int minutes) {
-		List<Player_stats> list=repository.findDataByMinutes(minutes);
+		List<Player_stats> list=repository.findByMinutes(minutes);
 		return list;
 	}
 
-	/*	For Filter Data By no Of score of goals  */
-	public List<Player_stats> getAllDataByScoreOfGoal(int goals) {
-		List<Player_stats> list=repository.findDataByScoreOfGoal(goals);
+	/*	For Filter Data By no Of Score Of Goals  */
+	public List<Player_stats> getAllDataByGoals(int goals) {
+		List<Player_stats> list=repository.findByGoals(goals);
 		return list;
 	}
 
-	/*	For Filter Data By Year Of Birth  */
-	public List<Player_stats> getAllDataByBirthYear(int birth_year) {
-		List<Player_stats> list=repository.findDataByBirthYear(birth_year);
+		
+	/*	For Filter Data By year Of Birth  */
+	public List<Player_stats> getAllDataByBirthYear(int birth) {
+		List<Player_stats> list=repository.findByBirth(birth);
 		return list;
 	}
-	 
 	
 	
 }
