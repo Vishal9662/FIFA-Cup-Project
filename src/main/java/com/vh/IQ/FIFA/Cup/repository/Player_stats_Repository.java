1291551c8251ba0f908b7c1c2a6e4dd2e8d8c51f
Vishal9662 <104097927+Vishal9662@users.noreply.github.com>
2022-12-16 16:34:03 +0530

    Update Player_stats_Repository.java

--- src/main/java/com/vh/IQ/FIFA/Cup/repository/Player_stats_Repository.java
+++ src/main/java/com/vh/IQ/FIFA/Cup/repository/Player_stats_Repository.java
@@ -1,6 +1,5 @@
 package com.vh.IQ.FIFA.Cup.repository;
 
-
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -11,13 +10,15 @@ import com.vh.IQ.FIFA.Cup.entity.Player_stats;
 @Repository
 public interface Player_stats_Repository extends JpaRepository<Player_stats, Integer>
 {
-	public List<Player_stats> findDataByGames(Integer games);
 
-	public List<Player_stats> findDataByMinutes(Integer minutes);
+	public List<Player_stats> findByGames(int games);
+
+	public List<Player_stats> findByMinutes(int minutes);
+
+	public List<Player_stats> findByGoals(int goals);
+
+	public List<Player_stats> findByBirth(int birth);
 	
-	public List<Player_stats> findDataByScoreOfGoal(Integer goals);
-	
-	public List<Player_stats> findDataByBirthYear(Integer birth_year);
 
 	
 }
